3e2b51bdbc178d5890fa1ad87a353d4acd11072f
uziel <dchatbot@e-bitware.com>
2020-08-24 21:42:58 -0500

    gunicorn

--- start_logs.py
+++ start_logs.py
@@ -9,9 +9,18 @@ def logs(app):
     app.logger.setLevel(logging.DEBUG)
     app.logger.addHandler(handler)
 
-    gunicorn_error_handlers = logging.getLogger('gunicorn.error').handlers
-    app.logger.handlers.extend(gunicorn_error_handlers)
+    handler = logging.handlers.RotatingFileHandler(
+        'log_gunicorn.txt',
+        maxBytes=1024 * 1024)
+    # only use gunicorn.error logger for all logging
+    LOGGER = logging.getLogger('gunicorn.error')
+    LOGGER.info('my info')
+    LOGGER.debug('debug message')
+    # this would write the log messages to error.log
+
+    #gunicorn_error_handlers = logging.getLogger('gunicorn.error').handlers
+    #app.logger.handlers.extend(gunicorn_error_handlers)
     #app.logger.addHandler(myhandler1)
     #app.logger.addHandler(myhandler2)
-    app.logger.info('my info')
-    app.logger.debug('debug message')
+    #app.logger.info('my info')
+    #app.logger.debug('debug message')
